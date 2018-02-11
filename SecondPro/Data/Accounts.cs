614ce7df82d1a781553ec5dfd2f13b21bc4bd641
LuckyTester152 <limak26@op.pl>
2018-02-11 11:22:37 +0100

    initial settings, EF code first, Accounts table

---
+++ SecondPro/Data/Accounts.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SecondPro.Data
+{
+    public class Accounts
+    {
+        public int Id { get; set; }
+        public decimal Balance { get; set; }
+    }
+}
---
+++ SecondPro/Data/ApplicationDbContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using SecondPro.Models;
+
+namespace SecondPro.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Accounts> Courses { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.Entity<Accounts>().ToTable("Accounts");
+        }
+    }
+}
